803c58d099adbe6ef979a41804fb570df4d1481b
Sebastián Gurín <sgurin@awalabs.com>
2019-07-25 01:04:11 -0300

    working emscripten scripts

---
+++ emscripten-scripts/disableConfigureLibraryCheck.js
@@ -0,0 +1,9 @@
+// var libs = ['zlib', 'freetype', 'png', 'jpeg']
+
+// have_zlib='no'
+// ZLIB_CFLAGS=""
+// ZLIB_LIBS=""
+// ZLIB_PKG=""
+// if test "x$with_zlib" = "xyes"; then
+//   AC_MSG_RESULT([-------------------------------------------------------------])
+//   PKG_CHECK_MODULES(ZLIB,[zl
